b1ade3997c197f8ef710952100743c8efb2cc973
DarkedgeTH <darkedgeth@gmail.com>
2022-03-11 10:16:50 -0500

    added onClick events to buttons

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function AppFunctional(props) {
 
+  const moveActive = (evt) => {
+
+  }
+
+  const resetGrid = () => {
+    
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
@@ -23,11 +31,11 @@ export default function AppFunctional(props) {
         <h3 id="message"></h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={moveActive}>LEFT</button>
+        <button id="up" onClick={moveActive}>UP</button>
+        <button id="right" onClick={moveActive}>RIGHT</button>
+        <button id="down" onClick={moveActive}>DOWN</button>
+        <button id="reset" onClick={resetGrid}>reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
