ab8639b7d0a592f3f12c9b4979b2c1188eac4531
DarkedgeTH <darkedgeth@gmail.com>
2022-03-11 13:20:01 -0500

    started setting up tests in App.test.js

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,17 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import AppFunctional from './AppFunctional';
+
+test('renders without errors', () => {
+  render(<AppFunctional/>)
+})
+
+test('renders the heading "Coordinates"', () => {
+  render(<AppFunctional/>)
+
+  const heading = screen.queryByText('Coordinates', { exact: false });
+
+  expect(heading).toBeVisible()
+  expect(heading).toBeInTheDocument()
 })
